c687772c3448eb393fe2b94d282a2d9e2de7624c
JITHU6 <33803989+JITHU6@users.noreply.github.com>
2019-01-31 09:17:40 +0530

    Registration
    Sample MVC

---
+++ RegistrationForm/RegistrationForm/Models/DemoRegister.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationForm.Models
+{
+    public class DemoRegister
+    {
+        public int DemoRegisterId { get; set; }
+
+        [Display(Name = "Name")]
+        [DataType(DataType.Text,ErrorMessage = "enter valid text")]
+        [Range(10,20)]
+        public string name { get; set; }
+
+        [Display(Name = "Mobile Number")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "enter valid number")]
+        public string mobilenumber { get; set; }
+
+
+        [Display(Name = "Address")]
+        [DataType(DataType.MultilineText, ErrorMessage = "enter Address")]
+        public string Address { get; set; }
+
+
+        [Display(Name = "Gender")]
+        [DataType(DataType.Text, ErrorMessage = "Enter gender")]
+        public string Gender { get; set; }
+
+
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "enter valid text")]
+        public string  Email { get; set; }
+
+
+        [Display(Name = "Course")]
+        [DataType(DataType.Text, ErrorMessage = "enter valid text")]
+        public string Course { get; set; }
+
+    }
+}
---
+++ RegistrationForm/RegistrationForm/Models/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace RegistrationForm.Models
+{
+    public class UserContext:DbContext
+    {
+        public UserContext()
+         :base("Mycon")
+        {
+                 DropCreateDatabaseIfModelChanges<UserContext> h=new DropCreateDatabaseIfModelChanges<UserContext>();
+                 Database.SetInitializer<UserContext>(h);
+         }
+        public DbSet<DemoRegister> users { get; set;}
+    }
+}
